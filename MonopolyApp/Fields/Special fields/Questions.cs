db480c5468954e98e9973419b4aa7400177cbfb5
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2021-01-16 00:09:58 +0200

    Добавил интерфейс IQuestion

---
+++ MonopolyApp/Fields/Special fields/Questions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using MonopolyApp.Interfaces;
+
+namespace MonopolyApp.Fields.Special_fields
+{
+    class Questions
+    {
+        
+    }
+}
---
+++ MonopolyApp/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using MonopolyApp.Interfaces;
+
+namespace MonopolyApp.Models
+{
+    class Question : IQuestion
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Coordinate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Image Appearance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
