7bd02569b46120758cb47712e459015df2145e10
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2021-01-29 00:39:26 +0200

    Добавил команды закрытия приложения и открытия игрового окна

--- MonopolyApp/ViewModels/StartWindowViewModel.cs
+++ MonopolyApp/ViewModels/StartWindowViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Navigation;
+using MonopolyApp.Infrastructure.Commands;
 using MonopolyApp.Infrastructure.Commands.Base;
+using MonopolyApp.View;
 using MonopolyApp.ViewModels.Base;
 
 namespace MonopolyApp.ViewModels
@@ -12,9 +15,47 @@ namespace MonopolyApp.ViewModels
     {
         public StartWindowViewModel()
         {
-            ICommand = new Command();
+            #region Commands
+            
+            ExitCommand = new LambdaCommand(OnExitApplicationCommandExecuted,CanExitApplicationCommandExecute);
+            OpenGameCommand =
+                new LambdaCommand(OnOpenGameApplicationCommandExecuted, CanOpenGameApplicationCommandExecute);
+
+            #endregion
+
         }
 
-        private ICommand ExitCommand;
+        #region Windows
+
+        private MainGame maingame;
+        
+
+        #endregion
+
+        #region Commands
+        public ICommand ExitCommand { get; }
+        public ICommand OpenGameCommand { get; }
+        
+        private void OnExitApplicationCommandExecuted(object p)
+        {
+            Application.Current.MainWindow.Close();
+        }
+        private bool CanExitApplicationCommandExecute(object p)
+        {
+            return true;
+        }
+
+        private void OnOpenGameApplicationCommandExecuted(object p)
+        {
+            maingame = new MainGame();
+            maingame.Show();
+        }
+        private bool CanOpenGameApplicationCommandExecute(object p)
+        {
+            return true;
+        }
+
+
+        #endregion
     }
 }
