55bf49857ca8c42f14bf478fe17d9fff90159d12
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2021-01-28 22:37:05 +0200

    Добавил базовую ViewModel

---
+++ MonopolyApp/ViewModels/StartWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Navigation;
+using MonopolyApp.ViewModels.Base;
+
+namespace MonopolyApp.ViewModels
+{
+    class StartWindowViewModel:ViewModel
+    {
+        public StartWindowViewModel()
+        {
+            _Title = "Начать игру!";
+        }
+
+        private string _Title;
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
